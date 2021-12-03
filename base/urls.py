ced3066e83ac7ab2e8fc2d7253f57e940d4cab2d
김윤혁 <kyhyeok@gmail.com>
2021-12-03 16:20:42 +0900

    Mobile Responsiveness

--- base/urls.py
+++ base/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
     path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
 
     path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
-    path('update-user/', views.udpateUser, name="update-user")
+    path('update-user/', views.udpateUser, name="update-user"),
 
+    path('topics/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
 ]
